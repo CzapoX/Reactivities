606a52d4b093f5abf1a2c60754c372224d609ea0
CzapoX <czapox@gmail.com>
2020-09-11 21:51:24 +0200

    End of Section 13

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { Menu, Container, Button } from "semantic-ui-react";
+import React, { useContext } from "react";
+import { Menu, Container, Button, Dropdown, Image } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
+  const rootStore = useContext(RootStoreContext);
+  const { isLoggedIn, user, logout } = rootStore.userStore;
 
   return (
     <Menu fixed="top" inverted>
@@ -24,6 +28,26 @@ export const NavBar = () => {
             content="Create Activity"
           />
         </Menu.Item>
+        {user && isLoggedIn && (
+          <Menu.Item position="right">
+            <Image
+              avatar
+              spaced="right"
+              src={user.image || "/assets/user.png"}
+            />
+            <Dropdown pointing="top left" text={user.displayName}>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  as={Link}
+                  to={`/profile/username`}
+                  text="My profile"
+                  icon="user"
+                />
+                <Dropdown.Item text="Logout" onClick={logout} icon="power" />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+        )}
       </Container>
     </Menu>
   );
